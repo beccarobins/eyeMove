4253944ffc4154f573ac2a66f0644a3bf5d1f848
Becca <35232346+beccarobins@users.noreply.github.com>
2018-08-12 17:13:01 -0400

    Create setup.py

---
+++ eyeMove/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eyeMove',
+      version='0.1',
+      description='A package for analyzing eye movements.',
+      url='http://github.com/beccarobins/eyeMove',
+      author='Becca Robins',
+      author_email='becca.k.robins@gmail.com',
+      license=None,
+      packages=['eyeMove'],
+      zip_safe=False)
